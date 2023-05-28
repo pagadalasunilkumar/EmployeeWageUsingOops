0bf44a6745c5faff319b7d63a976a2b9dbf40c29
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-28 10:00:07 +0530

    [sunil] the file is added

---
+++ UC-1Attendence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EmployeeWageComputationUsingOops
+{
+    internal class Employee
+    {
+        private bool isPresent;
+
+        public Employee()
+        {
+            Random random = new Random();
+            isPresent = random.Next(0, 2) == 0 ? false : true;
+        }
+
+        public void CheckAttendance()
+        {
+            if (isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            Employee employee = new Employee();
+            employee.CheckAttendance();
+        }
+    }
+}
