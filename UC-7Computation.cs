0c2308405ce202ddeb02ecf609db1afd2155be38
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-30 09:28:40 +0530

    [sunil] the file is added

---
+++ UC-7Computation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationUsingOops
+{
+    internal class UC_7Computation
+    {
+        public class PartTimeEmployee
+        {
+            private static int workingDaysPerMonth = 20;
+            private static int workingHoursPerMonth = 100;
+
+            private string name;
+            private double wage;
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            public double Wage
+            {
+                get { return wage; }
+                set { wage = value; }
+            }
+
+            public static double ComputeEmployeeWage()
+            {
+                double hourlyWage = workingHoursPerMonth > 0 ? wage / workingHoursPerMonth : 0;
+                double totalWage = hourlyWage * workingHoursPerMonth;
+                return totalWage;
+            }
+        }
+
+    }
+}
