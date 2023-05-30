cd4bad7cad2baae17c6fd71886e76a90beb08ec4
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-30 09:22:11 +0530

    [sunil] the file is added

---
+++ UC-5ForAMonth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationUsingOops
+{
+    internal class UC_5ForAMonth
+    {
+        public class PartTimeEmployee
+        {
+            private string name;
+            private double wage;
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            public double Wage
+            {
+                get { return wage; }
+                set { wage = value; }
+            }
+
+            public double CalculateMonthlyWage()
+            {
+                int workingDays = 20;  // Assuming 20 working days in a month
+                double monthlyWage = wage * workingDays;
+                return monthlyWage;
+            }
+        }
+
+        
+    }
+}
